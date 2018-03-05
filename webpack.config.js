5de509df491f3bc22ff0279a5925eb45a623aec7
akarienta <akarienta@gmail.com>
2018-03-05 16:01:39 +0100

    WDS added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'Webpack demo'
-		})
-	]
+		}),
+		new webpack.WatchIgnorePlugin([path.join(__dirname, 'node_modules')]),
+		new WebpackNotifierPlugin(),
+		new FriendlyErrorsWebpackPlugin()
+	],
+
+	devServer: {
+		stats: 'errors-only',
+		host: process.env.HOST,
+		port: process.env.PORT,
+		overlay: {
+			errors: true,
+			warnings: true
+		},
+		watchOptions: {
+			aggregateTimeout: 300,
+			poll: 1000
+		}
+	}
 };
